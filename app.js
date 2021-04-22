b640ef034aa6538a553d71aee6f88c923b2fef04
Jan van Hest <jan@travpromobile.com>
2021-04-22 22:02:37 +0200

    Finished simple vue.js example.

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const app = Vue.createApp({
       picture: "https://randomuser.me/api/portraits/men/10.jpg",
     };
   },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api/");
+      const { results } = await res.json();
+      const { name, email, gender, picture } = results[0];
+      console.log(results);
+      this.firstName = name.first;
+      this.lastName = name.last;
+      this.email = email;
+      this.gender = gender;
+      this.picture = picture.large;
+    },
+  },
 });
 
 app.mount("#app");
